e3b2e028909d16247dcc4097f29fcb85b66d6b83
Lou Jacoubs <lou.jacoubs@kellogg.northwestern.edu>
2023-05-24 14:54:59 +0000

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,10 +9,15 @@ class PostsController < ApplicationController
     end
   
     def create
+      if @current_user
       @post = Post.new
       @post["body"] = params["post"]["body"]
       @post["image"] = params["post"]["image"]
+      @post["user_id"] = @current_user["id"]
       @post.save
+      else
+        flash["notice"] = "Login first"
+      end     
       redirect_to "/posts"
     end
   
